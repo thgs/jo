12135f1253fcbbf55d8021f777da76d2bf101bb6
thgs <theogpl57@gmail.com>
2018-08-21 22:05:06 +0300

    added laravel-imap providers and published config

--- config/app.php
+++ config/app.php
@@ -151,6 +151,8 @@ return [
          * Package Service Providers...
          */
 
+         Webklex\IMAP\Providers\LaravelServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -208,6 +210,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Client' => Webklex\IMAP\Facades\Client::class,
 
     ],
 
