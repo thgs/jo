380de3e05b822febc0effea8e9ee7227bb3a1360
thgs <theogpl57@gmail.com>
2018-08-21 22:57:24 +0300

    added draft EmailAccount model

---
+++ database/migrations/2018_08_21_191759_create_email_accounts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_accounts', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('username');
+            $table->string('password');
+            $table->string('host');
+            $table->string('port');
+            $table->string('encryption');
+            $table->string('protocol');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('email_accounts');
+    }
+}
