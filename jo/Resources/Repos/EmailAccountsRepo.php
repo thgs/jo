5f936855ad506aae01a28c4f0cba03d43fc09170
thgs <theogpl57@gmail.com>
2018-09-06 23:51:51 +0300

    repo/EmailAccounts adds user_id if not present from Auth

--- jo/Resources/Repos/EmailAccountsRepo.php
+++ jo/Resources/Repos/EmailAccountsRepo.php
@@ -2,6 +2,7 @@
 
 namespace Jo\Resources\Repos;
 
+use Auth;
 use App\Models\EmailAccount;
 use Jo\Abstracts\AbstractRepository;
 
@@ -10,5 +11,19 @@ class EmailAccountsRepo extends AbstractRepository
     public function __construct(EmailAccount $model)
     {
         parent::__construct($model);
+    }
+
+
+    public function create($data)
+    {
+        // we check if user_id is set
+        if (!isset($data['user_id']))
+        {
+            // and set it if we have to
+            $data['user_id'] = \Auth::user()->id;
+        }
+
+        // return the model
+        return $this->model->create($data);
     }
 }
