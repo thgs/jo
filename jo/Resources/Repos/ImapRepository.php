65d9b6e3ef10a3e2b2c86abd4639d35fb6e75314
thgs <theogpl57@gmail.com>
2018-09-09 23:57:48 +0300

    few additions on ImapRepository

--- jo/Resources/Repos/ImapRepository.php
+++ jo/Resources/Repos/ImapRepository.php
@@ -34,16 +34,43 @@ class ImapRepository
     }
 
 
-    public function getUnseenMessages()
+    public function getUnseenMessages($folder = 'INBOX', $criteria = 'unseen')
     {
         // get inbox messages
-        $inbox = $this->client->getFolder('INBOX');
+        $inbox = $this->client->getFolder($folder);
         return $this->client->getUnseenMessages(
             $inbox,
-            'unseen',
+            $criteria,
             false,
             false,
             true
         );
+    }
+
+    public function getFolderMessages($folder = 'INBOX', $criteria = 'all')
+    {
+        $folder = $this->client->getFolder($folder);
+        return $this->client->getMessages(
+            $folder,
+            $criteria,
+            false,
+            false,
+            true
+        );
+    }
+
+    // this kinda escapes the repository boundaries..
+    // but can be convenient for
+    public function __call($method, $parameters)
+    {
+        if (method_exists($this->client, $method))
+            $this->client->$method(...$parameters);
+        else
+            throw new \Exception(__CLASS__.': '.$method.' is not defined!');
+    }
+
+    public function __sleep()
+    {
+        $this->client->disconnect();
     }
 }
