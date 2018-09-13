084eaf236aae53e77547eefea25717e372d6e3df
thgs <theogpl57@gmail.com>
2018-09-14 02:45:40 +0300

    draft making it work

--- jo/Resources/Repos/EmailsRepo.php
+++ jo/Resources/Repos/EmailsRepo.php
@@ -12,25 +12,30 @@ class EmailsRepo extends AbstractRepository
         parent::__construct($model);
     }
 
-    public function storeBulkFromImap($messages, $emailAccountId)
+    public function storeBulkFromImap($messages, $emailAccountId, $mailbox = 'INBOX')
     {
         $models = [];
         foreach ($messages as $m)
         {
+            // here is where i think laravel-imap has some issues..
 
+            // we need to call this before asking for bodies
+            $m->parseBody();
             $data = [
                 'uid'       => $m->getUid(),
-                'mailbox'   => $m->getContainingFolder()->name,
-                'from'      => $m->getFrom(),
-                'cc'        => $m->getCc(),
-                'bcc'       => $m->getBcc(),
-                'to'        => $m->getTo(),
-                'reply_to'  => $m->getReplyTo(),
+                // changed to $mailbox, since $m->getContainingFolder doesnt
+                // seem to work (tries localhost ?)
+                'mailbox'   => $mailbox,
+                'from'      => $m->getFrom()[0]->full,
+                'cc'        => $this->consolidate_array($m->getCc()),
+                'bcc'       => $this->consolidate_array($m->getBcc()),
+                'to'        => $this->consolidate_array($m->getTo()),
+                'reply_to'  => $this->consolidate_array($m->getReplyTo()),
                 'subject'   => $m->getSubject(),
                 'body'      => $m->hasHTMLBody()
                                 ? $m->getHTMLBody()
                                 : $m->getTextBody(),
-                'flags'     => $m->getFlags(),
+                'flags'     => (string) $m->getFlags(),
                 'priority'  => $m->getPriority(),
 
                 'email_account_id' => $emailAccountId,
@@ -40,5 +45,14 @@ class EmailsRepo extends AbstractRepository
         }
 
         return $models;
+    }
+
+    private function consolidate_array(array $a)
+    {
+        $fulls = array_map(
+            function ($x) { return $x->full ?? ''; },
+            $a
+        );
+        return implode('|', $fulls);
     }
 }
