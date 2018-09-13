ee3e7c6e431caf34e4ddadfeeffa182be3e8f632
thgs <theogpl57@gmail.com>
2018-09-13 03:04:52 +0300

    added a draft of EmailsSynchroniser

---
+++ jo/Synchronisation/EmailsSynchroniser.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Jo\Synchronisation;
+
+use Jo\Resources\Repos\EmailsRepo;
+use Jo\Resources\Repos\ImapRepository;
+
+class EmailsSynchroniser
+{
+
+    protected $emailsRepo;
+    protected $emailAccountsRepo;
+
+    public function __construct(EmailsRepo $emailsRepo, EmailAccountsRepo $repo)
+    {
+        $this->emailsRepo = $emailsRepo;
+        $this->emailAccountsRepo = $repo;
+    }
+
+    public function syncAll()
+    {
+        $accounts = $this->emailAccountsRepo->getAccountsToSync();
+
+        foreach ($accounts as $account)
+        {
+            $imapRepo = new ImapRepository($account);
+
+            $newMessages = $imapRepo->getUnseenMessages();
+
+            // let's not handle real synchronisation, for now it will be
+            // okay, just store the unseen
+            $this->emailsRepo->storeBulkFromImap($newMessages);
+
+            // update the account, saying we have synced
+            $account->touch();
+        }
+
+    }
+}
