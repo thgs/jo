7175fcf4e99bbec26b6c3c178d8a7e1100d4268c
thgs <theogpl57@gmail.com>
2018-09-19 00:01:06 +0300

    fixed link for logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="/logo.png" style="max-width: 32px" />
+                    <img src="{{ asset('logo.png') }}" style="max-width: 32px" />
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
