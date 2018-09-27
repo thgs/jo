12652fbfe47e53bb4a63b842a49f6c5f64d3b4e7
thgs <theogpl57@gmail.com>
2018-09-27 11:12:02 +0300

    cleaned up and added comments

--- tests/Feature/SynchroniseFeedsTest.php
+++ tests/Feature/SynchroniseFeedsTest.php
@@ -14,6 +14,7 @@ class SynchroniseFeedsTest extends TestCase
 
     protected $feed;
 
+
     public function setUp()
     {
         parent::setUp();
@@ -32,88 +33,74 @@ class SynchroniseFeedsTest extends TestCase
      */
     public function testRetrievesFeedEntries()
     {
-        // given
-        // we have a real feed from setUp
+        // given -- we have a real feed from setUp
 
-        // below should go to FeedTest
-        //$this->assertTrue($feed instanceof Feed);
-        //$this->assertDatabaseHas('feeds', ['name' => 'Reuters / Arts']);
-
-        // when
-        // we call artisan command feeds:sync
-        // we make sure we sleep for 1 sec, so as to give laravel the possibility
-        // to update the feed.updated_at at the next second
+        // when -- we call artisan command feeds:sync
         $this->callSync(1);
 
-        // then
-        // we see updated time in feed change
+        // then -- we see updated time in feed change
         $this->assertDatabaseMissing('feeds', [
             'id' => $this->feed->id,
-            'updated_at' => $this->feed->created_at,   // we should not have same time
+            'updated_at' => $this->feed->created_at,  // we should not have same time
         ]);
 
-        // we see entries in database, in Posts
+        // we also see entries in database, in Posts
         $this->assertTrue($this->feed->posts()->count() > 0);
         $this->assertDatabaseHas('posts', ['feed_id' => $this->feed->id]);
     }
 
 
     /**
-     * Checks if we can retrieve feed entries
+     * Checks that Synchroniser does not store duplicates
      *
      * @return void
      */
     public function testDoesNotTryToStorePostsTwice()
     {
-        // given
-        // we have a feed from setUp
+        // given -- we have a feed from setUp
 
-        // when
-        // we sync
+        // when -- when we sync..
         $this->callSync(1);
 
         $postsInDbFirstSync = $this->feed->posts()->count();
 
-        // if we try sync again after 1 second
+        // .. if we try sync again after 1 second
         $this->callSync(1);
 
-        // then
-        // posts should be the same, except if we are so unlucky and it did
-        // update this very second !
+        // then -- posts in database should be the same, except if we are so
+        // unlucky and the feed updated its content
         $this->assertEquals($postsInDbFirstSync, $this->feed->posts()->count());
     }
 
     /**
-     * Checks if we can retrieve feed entries
+     * Checks if console output is fine
      *
      * @return void
      */
     public function testDisplaysOutput()
     {
-        // this should be better in 5.7
+        // todo: this should be better in 5.7
 
-        // given
-        // we have a feed.. from setUp
+        // given -- we have a feed.. from setUp
 
-        // when
-        // we sync
+        // when -- we sync
         $this->callSync(1);
 
+        // then -- the output in console should include the posts count and
+        // the feed name
         $postsInDbFirstSync = $this->feed->posts()->count();
 
         $output['first'] = Artisan::output();
-        //dump([$output, $postsInDbFirstSync]);
 
         $this->assertTrue(strpos($output['first'], $this->feed->name) !== false);
         $this->assertTrue(strpos($output['first'], (string) $postsInDbFirstSync) !== false);
 
 
-        // if we try sync again after 1 second
+        // and if we try to sync again after 1 second, the output should show
+        // 0 on new entries, as long as the feed itself hasn't put new content
         $this->callSync(1);
 
         $output['second'] = Artisan::output();
-
-        //dump([$output, $postsInDbFirstSync]);
 
         $this->assertTrue(strpos($output['second'], $this->feed->name) !== false);
         $this->assertEquals($postsInDbFirstSync, $this->feed->posts()->count());
@@ -121,27 +108,35 @@ class SynchroniseFeedsTest extends TestCase
     }
 
     /**
-     * Checks if we can retrieve feed entries
+     * Checks if an Exception is thrown given an invalid URL
      *
      * @return void
      */
     public function testThrowsExceptionIfInvalidUrl()
     {
-        // given
-        // we have a feed..
+        // given -- we have a feed with wrong url
         $feed = factory(\App\Models\Feed::class)->create([
             'name' => 'Reuters / Arts',
             'url' => 'http://somethingverywrong',
         ]);
 
+        // an exception will be thrown
         $this->expectException(\Exception::class);
 
+        // when we call sync
         $this->callSync(1);
     }
 
 
+    /**
+     * Calls feeds:sync command
+     *
+     * @param int delayInSeconds    Number of seconds to sleep before executing
+     */
     private function callSync($delayInSeconds = 0)
     {
+        // make sure we sleep for an amount of time, so as to make possible
+        // to update the feed.updated_at at a different time than creation
         sleep($delayInSeconds);
         Artisan::call('feeds:sync');
     }
