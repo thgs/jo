c358cf52f2b1691bd57670699f811954e172d2fe
thgs <theogpl57@gmail.com>
2018-10-05 02:09:01 +0300

    actually adding the 5th driver

--- jo/IMAP/Drivers/PhpImap.php
+++ jo/IMAP/Drivers/PhpImap.php
@@ -2,33 +2,36 @@
 
 namespace Jo\IMAP\Drivers;
 
+use PhpImap\Mailbox;
+
 class PhpImapDriver extends BaseDriver implements JoImapDriver
 {
 	public function boot()
 	{
-		$this->setClient(new Client([
-            'host'          => $account->host,
-            'port'          => $account->port,
-            'encryption'    => $account->encryption,
-            'validate_cert' => true,
-            'username'      => $account->username,
-            'password'      => $account->password,
-            'protocol'      => $account->protocol,
-        ]));
+		$this->setClient(
+			new Mailbox(
+				sprintf('{%s:%s/imap/ssl}INBOX', $this->account->host, $this->account->port),
+				$this->account->username,
+				$this->account->password,
+				storage_path()		// save attachments dir
+			);
+		);
 	}
 
 	public function getFolders()
 	{
-
+		$this->client->getListingFolders();
 	}
 
 	public function getUnseenMessages($folder = 'INBOX', $criteria = 'unseen')
 	{
-
+		return $this->getFolderMessages($folder, 'new');
 	}
 
 	public function getFolderMessages($folder = 'INBOX', $criteria = 'all')
 	{
+		$this->client->switchMailbox($folder);
 		
+		return $this->client->searchMailbox(strtoupper($criteria));
 	}
 }
